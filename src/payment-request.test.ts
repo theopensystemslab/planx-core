05eacee248ee929554464d456a0cfb4a81a45c52
Benjamin Williams <benjamin@opensystemslab.io>
2023-04-13 09:34:18 +0100

    Update createPaymentRequest for applicant_name and payment_amount

--- src/payment-request.test.ts
+++ src/payment-request.test.ts
@@ -1,7 +1,7 @@
-import { validateSessionData } from "./payment-request";
+import { extractSessionPreviewData } from "./payment-request";
 import type { Session, KeyPath } from "./types";
 
-describe("validateSessionData", () => {
+describe("extractSessionPreviewData", () => {
   test("passport data must be available", () => {
     const emptySession: Session = {
       id: "abc",
@@ -13,80 +13,9 @@ describe("validateSessionData", () => {
       },
     };
     const previewKeys: KeyPath[] = [];
-    const paymentAmountKey = "amount";
-    expect(() =>
-      validateSessionData({
-        session: emptySession,
-        sessionPreviewKeys: previewKeys,
-        paymentAmountKey,
-      })
-    ).toThrow("passport data not found");
-  });
-  test("amount must not be undefined", () => {
-    const invalidSession: Session = {
-      id: "abc",
-      flowId: "abc",
-      data: {
-        id: "abc",
-        passport: {
-          data: {},
-        },
-        breadcrumbs: {},
-      },
-    };
-    const previewKeys: KeyPath[] = [];
-    const paymentAmountKey = "amount";
-    expect(() =>
-      validateSessionData({
-        session: invalidSession,
-        sessionPreviewKeys: previewKeys,
-        paymentAmountKey,
-      })
-    ).toThrow('invalid paymentAmount: "undefined"');
-  });
-  test("amount must be greater than zero", () => {
-    const invalidSession: Session = {
-      id: "abc",
-      flowId: "abc",
-      data: {
-        id: "abc",
-        passport: {
-          data: {
-            amount: -2,
-          },
-        },
-        breadcrumbs: {},
-      },
-    };
-    const previewKeys: KeyPath[] = [];
-    const paymentAmountKey = "amount";
-    expect(() =>
-      validateSessionData({
-        session: invalidSession,
-        sessionPreviewKeys: previewKeys,
-        paymentAmountKey,
-      })
-    ).toThrow('invalid paymentAmount: "-2"');
-    const invalidSession2: Session = {
-      id: "abc",
-      flowId: "abc",
-      data: {
-        id: "abc",
-        passport: {
-          data: {
-            amount: 0,
-          },
-        },
-        breadcrumbs: {},
-      },
-    };
-    expect(() =>
-      validateSessionData({
-        session: invalidSession2,
-        sessionPreviewKeys: previewKeys,
-        paymentAmountKey,
-      })
-    ).toThrow('invalid paymentAmount: "0"');
+    expect(() => extractSessionPreviewData(emptySession, previewKeys)).toThrow(
+      "passport data not found"
+    );
   });
   test("keys must be present in the passport", () => {
     const invalidSession: Session = {
@@ -97,20 +26,14 @@ describe("validateSessionData", () => {
         passport: {
           data: {
             key1: "a",
-            amount: 2,
           },
         },
         breadcrumbs: {},
       },
     };
     const previewKeys: KeyPath[] = [["key1"], ["key2", "notFoundKey"]];
-    const paymentAmountKey = "amount";
     expect(() =>
-      validateSessionData({
-        session: invalidSession,
-        sessionPreviewKeys: previewKeys,
-        paymentAmountKey,
-      })
+      extractSessionPreviewData(invalidSession, previewKeys)
     ).toThrow('passport key "key2.notFoundKey" not found in passport data');
   });
   test("a simple set of session preview keys are extracted from the session", () => {
@@ -124,7 +47,6 @@ describe("validateSessionData", () => {
             a: 1,
             b: 2,
             c: 3,
-            amount: 5,
           },
         },
         breadcrumbs: {},
@@ -132,14 +54,11 @@ describe("validateSessionData", () => {
     };
     const previewKeys: KeyPath[] = [["a"], ["b"], ["c"]];
 
-    const validatedSessionData = validateSessionData({
-      session: session,
-      sessionPreviewKeys: previewKeys,
-      paymentAmountKey: "amount",
-    });
-    expect(validatedSessionData).toEqual({
-      paymentAmount: 5,
-      sessionPreviewData: { a: 1, b: 2, c: 3 },
+    const sessionPreviewData = extractSessionPreviewData(session, previewKeys);
+    expect(sessionPreviewData).toEqual({
+      a: 1,
+      b: 2,
+      c: 3,
     });
   });
   test("a set of compound keys are extracted from the session", () => {
@@ -153,22 +72,14 @@ describe("validateSessionData", () => {
             "a.b": 1,
             "c.d": 2,
             "c.d.e": 3,
-            "payment.amount": 7,
           },
         },
         breadcrumbs: {},
       },
     };
     const previewKeys: KeyPath[] = [["a.b"], ["c.d"], ["c.d.e"]];
-    const validatedSessionData = validateSessionData({
-      session: session,
-      sessionPreviewKeys: previewKeys,
-      paymentAmountKey: "payment.amount",
-    });
-    expect(validatedSessionData).toEqual({
-      paymentAmount: 7,
-      sessionPreviewData: { "a.b": 1, "c.d": 2, "c.d.e": 3 },
-    });
+    const sessionPreviewData = extractSessionPreviewData(session, previewKeys);
+    expect(sessionPreviewData).toEqual({ "a.b": 1, "c.d": 2, "c.d.e": 3 });
   });
   test("a set of nested and compound keys are extracted from the session", () => {
     const session: Session = {
@@ -186,7 +97,6 @@ describe("validateSessionData", () => {
             b: {
               "c.d.e.f": false,
             },
-            "payment.amount": 7,
           },
         },
         breadcrumbs: {},
@@ -198,21 +108,14 @@ describe("validateSessionData", () => {
       ["a", "c.d.e"],
       ["b", "c.d.e.f"],
     ];
-    const validatedSessionData = validateSessionData({
-      session: session,
-      sessionPreviewKeys: previewKeys,
-      paymentAmountKey: "payment.amount",
-    });
-    expect(validatedSessionData).toEqual({
-      paymentAmount: 7,
-      sessionPreviewData: {
-        a: {
-          c: 0,
-          "c.d": true,
-          "c.d.e": 5,
-        },
-        b: { "c.d.e.f": false },
+    const sessionPreviewData = extractSessionPreviewData(session, previewKeys);
+    expect(sessionPreviewData).toEqual({
+      a: {
+        c: 0,
+        "c.d": true,
+        "c.d.e": 5,
       },
+      b: { "c.d.e.f": false },
     });
   });
 });
