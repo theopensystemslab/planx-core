16ec67991987c9414dbf87b5e53b6e7c82b048e1
Ben <109954107+builditben@users.noreply.github.com>
2023-04-26 08:53:08 +0100

    refactor: tidy type declarations (#15)
    This PR moves the definition of shared type definitions into their own
    folder where they can be exposed.
    
    This enables usage like import type { ... } from
    "@opensystemslab/planx-core/types" and prevents having to re-export from
    the index which isn't a nice pattern.
    
    Shared types have also been split into sensible groupings of individual
    files to make maintenance easier.
    
    Implemented in corresponding planx PR:
    https://github.com/theopensystemslab/planx-new/pull/1666

---
+++ src/types/session.ts
@@ -0,0 +1,50 @@
+import type { Value } from "./data";
+import { ComponentType } from "./component";
+import type { GovUKPayment } from "./gov-uk-payment";
+
+export interface SessionData {
+  passport: Passport;
+  breadcrumbs: Breadcrumbs;
+  govUkPayment?: GovUKPayment;
+  id: string;
+}
+
+export interface Session {
+  data: SessionData;
+  id: string;
+  flowId: string;
+}
+
+export interface DetailedSession extends Session {
+  lockedAt: string;
+}
+
+export interface Crumb {
+  auto?: boolean;
+  answers?: Array<string>;
+  data?: Record<string, Value>;
+  override?: Record<string, Value>;
+  feedback?: string;
+}
+
+export type Breadcrumbs = {
+  [key: string]: Crumb;
+};
+
+export interface NormalizedCrumb extends Crumb {
+  id: string;
+  type: ComponentType;
+  autoAnswered: boolean;
+  sectionId?: string;
+  answers?: Array<string>;
+  data?: Record<string, Value>;
+  override?: Record<string, Value>;
+  feedback?: string;
+}
+
+export type OrderedBreadcrumbs = Array<NormalizedCrumb>;
+
+// TODO: This should be Record<string, Value>;
+export interface Passport {
+  data?: Record<string, any>;
+}
