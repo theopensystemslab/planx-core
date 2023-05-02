13e0de2541a7590e481599857336270e63bfd160
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-05-02 11:55:34 +0200

    use toStrictEqual on payload checks

--- src/export/bops/bops.test.ts
+++ src/export/bops/bops.test.ts
@@ -4,23 +4,21 @@ import { mockSessionData } from "./mocks/sessionData";
 import { getBOPSParams } from ".";
 import { DEFAULT_APPLICATION_TYPE, USER_ROLES } from "./model";
 
-describe("BOPS payload", () => {
-  const sessionId = "969b912c-f196-4ec6-ac73-133f2a516f60";
-  const flowName = "Apply for a lawful development certificate";
+describe("Full BOPS payload", () => {
   const generatedPayload = getBOPSParams({
     breadcrumbs: mockSessionData.breadcrumbs,
     flow: mockPublishedLDCFlow,
     passport: mockSessionData.passport,
-    sessionId: sessionId,
-    flowName: flowName,
+    sessionId: "969b912c-f196-4ec6-ac73-133f2a516f60",
+    flowName: "Apply for a lawful development certificate",
   });
 
-  it("builds the full payload as expected", () => {
+  it("builds the payload as expected", () => {
     // check the response shape excluding proposal_details as order can vary
     const generatedMinimumPayload = (({ proposal_details, ...p }) => p)(generatedPayload);
     const mockExpectedMinimumPayload = (({ proposal_details, ...p}) => p)(mockExpectedBOPSPayload);
 
-    expect(generatedMinimumPayload).toEqual(mockExpectedMinimumPayload);
+    expect(generatedMinimumPayload).toStrictEqual(mockExpectedMinimumPayload);
   });
 
   it("builds proposal_details as expected", () => {
@@ -29,8 +27,8 @@ describe("BOPS payload", () => {
   });
 
   it("attaches planx_debug_data to the payload that matches the raw session data", () => {
-    expect(generatedPayload.planx_debug_data?.passport).toEqual(mockSessionData?.passport);
-    expect(generatedPayload.planx_debug_data?.breadcrumbs).toEqual(mockSessionData?.breadcrumbs);
+    expect(generatedPayload.planx_debug_data?.passport).toStrictEqual(mockSessionData?.passport);
+    expect(generatedPayload.planx_debug_data?.breadcrumbs).toStrictEqual(mockSessionData?.breadcrumbs);
   });
 });
 
