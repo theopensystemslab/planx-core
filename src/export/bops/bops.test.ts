501135384e80d5287fef84fd02ca801788d3ff03
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-05-02 10:46:31 +0200

    tidy up

--- src/export/bops/bops.test.ts
+++ src/export/bops/bops.test.ts
@@ -4,7 +4,7 @@ import { mockSessionData } from "./mocks/sessionData";
 import { getBOPSParams } from ".";
 import { DEFAULT_APPLICATION_TYPE, USER_ROLES } from "./model";
 
-describe("Full BOPS payload", () => {
+describe("BOPS payload", () => {
   const sessionId = "969b912c-f196-4ec6-ac73-133f2a516f60";
   const flowName = "Apply for a lawful development certificate";
   const generatedPayload = getBOPSParams({
@@ -15,16 +15,12 @@ describe("Full BOPS payload", () => {
     flowName: flowName,
   });
 
-  it("builds the minimum payload as expected", () => {
-    // check the response shape excluding result, proposal_details & planx_debug_data (these get tested separately)
-    const generatedMinimumPayload = (({ result, proposal_details, planx_debug_data, ...p }) => p)(generatedPayload);
-    const mockExpectedMinimumPayload = (({ result, proposal_details, planx_debug_data, ...p}) => p)(mockExpectedBOPSPayload);
+  it("builds the full payload as expected", () => {
+    // check the response shape excluding proposal_details as order can vary
+    const generatedMinimumPayload = (({ proposal_details, ...p }) => p)(generatedPayload);
+    const mockExpectedMinimumPayload = (({ proposal_details, ...p}) => p)(mockExpectedBOPSPayload);
 
     expect(generatedMinimumPayload).toEqual(mockExpectedMinimumPayload);
-  });
-
-  it("builds the correct result as expected", () => {
-    expect(generatedPayload.result).toEqual(mockExpectedBOPSPayload.result);
   });
 
   it("builds proposal_details as expected", () => {
@@ -33,10 +29,7 @@ describe("Full BOPS payload", () => {
   });
 
   it("attaches planx_debug_data to the payload that matches the raw session data", () => {
-    expect(generatedPayload.planx_debug_data?.passport).toEqual(mockExpectedBOPSPayload.planx_debug_data?.passport);
     expect(generatedPayload.planx_debug_data?.passport).toEqual(mockSessionData?.passport);
-
-    expect(generatedPayload.planx_debug_data?.breadcrumbs).toEqual(mockExpectedBOPSPayload.planx_debug_data?.breadcrumbs);
     expect(generatedPayload.planx_debug_data?.breadcrumbs).toEqual(mockSessionData?.breadcrumbs);
   });
 });
