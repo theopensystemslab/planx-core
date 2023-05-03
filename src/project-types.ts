077fe4b6681e1d9cd0a67ed3f1ec4f5f3a47b7cc
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-05-03 12:37:45 +0100

    feat: Human readable project types (#20)

---
+++ src/project-types.ts
@@ -0,0 +1,42 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+interface ProjectType {
+  description: string
+}
+
+export async function formatRawProjectTypes(
+  client: GraphQLClient,
+  rawProjectTypes: string[]
+): Promise<string> {
+  const humanReadableProjectTypes = await lookupHumanReadableProjectTypes(client, rawProjectTypes);
+  const formattedProjectTypes = formatHumanReadableProjectTypes(humanReadableProjectTypes);
+  return formattedProjectTypes;
+}
+
+async function lookupHumanReadableProjectTypes(
+  client: GraphQLClient,
+  rawList: string[]
+): Promise<string[]> {
+  const query = gql`
+    query LookupHumanReadableProjectType($rawList: [String!]) {
+      projectTypes: project_types(where: { value: { _in: $rawList } }) {
+        description
+      }
+    }
+  `;
+  const { projectTypes } = await client.request<{ projectTypes: ProjectType[]}>(query, { rawList });
+  const humanReadableProjectTypes = projectTypes.map((result) => result.description);
+  return humanReadableProjectTypes;
+}
+
+export function formatHumanReadableProjectTypes(
+  projectTypes: string[],
+): string {
+  // Join in readable format - en-US ensures we use Oxford commas
+  const formatter = new Intl.ListFormat("en-US", { type: "conjunction" });
+  const joinedList = formatter.format(projectTypes);
+  // Convert first character to uppercase
+  const formattedProjectTypes =
+    joinedList.charAt(0).toUpperCase() + joinedList.slice(1);
+  return formattedProjectTypes;
+}
