9204229f7cbcaa7d9ad55d3d2fd17bda0df88c42
Benjamin Williams <benjamin@opensystemslab.io>
2023-07-10 15:44:42 +0100

    Add template export types

--- src/types/export.ts
+++ src/types/export.ts
@@ -9,3 +9,18 @@ export type ExportData = {
   responses: QuestionAndResponses[];
   redactedResponses: QuestionAndResponses[];
 };
+
+export interface PolicyRefs {
+  text: string;
+}
+
+export interface PlanXExportData {
+  question: string;
+  responses: unknown;
+  metadata?: {
+    portal_name?: string;
+    section_name?: string;
+    policy_refs?: PolicyRefs[];
+    flags?: string[];
+  };
+}
