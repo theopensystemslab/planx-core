d956e78b1718d670f17884b3d9497ad1370e52d3
Benjamin Williams <benjamin@opensystemslab.io>
2023-07-18 09:13:09 +0100

    Change html generation to output a static string

--- src/templates/generateExamples.ts
+++ src/templates/generateExamples.ts
@@ -7,14 +7,15 @@ import {
 import { Packer } from "docx";
 import {
   generateDocxTemplateStream,
-  generateMapHTMLStream,
-  generateApplicationHTMLStream,
+  generateMapHTML,
+  generateApplicationHTML,
 } from "./index";
 import { buildTestTemplate } from "./docx/testTemplate";
 import exampleLDCEData from "./mocks/exampleLDCE.json";
 import exampleLDCPData from "./mocks/exampleLDCP.json";
 import exampleData from "./mocks/example.json";
 import exampleSectionData from "./mocks/exampleWithSections.json";
+import type { Passport } from "../types";
 
 (async () => {
   try {
@@ -34,28 +35,14 @@ async function setUpExampleDir() {
 }
 
 async function generateHTMLExamples() {
-  const data = exampleData.data;
-  const sectionData = exampleSectionData.data;
-  const geojson = exampleData.geojson;
+  const applicationHTML = generateApplicationHTML(exampleData.data);
+  writeFileSync(`./examples/application.html`, applicationHTML);
 
-  const promises: Promise<void>[] = [];
+  const sectionHTML = generateApplicationHTML(exampleSectionData.data);
+  writeFileSync(`./examples/application_with_sections.html`, sectionHTML);
 
-  const htmlFile = createWriteStream(`./examples/application.html`);
-  const htmlStream = generateApplicationHTMLStream(data).pipe(htmlFile);
-  promises.push(resolveStream(htmlStream));
-
-  const sectionHtmlFile = createWriteStream(
-    `./examples/application_with_sections.html`,
-  );
-  const sectionHtmlStream =
-    generateApplicationHTMLStream(sectionData).pipe(sectionHtmlFile);
-  promises.push(resolveStream(sectionHtmlStream));
-
-  const mapFile = createWriteStream(`./examples/map.html`);
-  const mapStream = generateMapHTMLStream(geojson).pipe(mapFile);
-  promises.push(resolveStream(mapStream));
-
-  await waitForAllOrExit(promises);
+  const mapHTML = generateMapHTML(exampleData.geojson);
+  writeFileSync(`./examples/map.html`, mapHTML);
 }
 
 async function generateTemplateExamples() {
@@ -65,20 +52,20 @@ async function generateTemplateExamples() {
   });
 
   // setup test data
-  const examples: Record<string, { data: any }> = {
-    LDCE: exampleLDCEData as { data: any },
-    LDCE_redacted: exampleLDCEData as { data: any },
-    LDCP: exampleLDCPData as { data: any },
-    LDCP_redacted: exampleLDCPData as { data: any },
+  const examples = {
+    LDCE: exampleLDCEData as Passport,
+    LDCE_redacted: exampleLDCEData as Passport,
+    LDCP: exampleLDCPData as Passport,
+    LDCP_redacted: exampleLDCPData as Passport,
   };
 
   // build templates
-  const promises: Promise<void>[] = Object.keys(examples).map(
-    async (templateName) => {
+  const promises: Promise<void>[] = Object.entries(examples).map(
+    async ([templateName, passport]) => {
       const file = createWriteStream(`./examples/${templateName}.docx`);
       const docStream = generateDocxTemplateStream({
         templateName,
-        passport: examples[templateName],
+        passport,
       }).pipe(file);
       return resolveStream(docStream);
     },
