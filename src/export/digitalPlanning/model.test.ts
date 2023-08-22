5844d695dd4d5683a2789c23716d4b76a9a94c6b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-22 17:02:01 +0100

    feat: Initial setup of new data export format (#100)

---
+++ src/export/digitalPlanning/model.test.ts
@@ -0,0 +1,117 @@
+import { Passport } from "../../models/passport";
+import { mockProposedLDCPassportData } from "../oneApp/mocks/passport";
+import { DigitalPlanning } from "./model";
+
+const mockPassport = new Passport({
+  data: {
+    ...mockProposedLDCPassportData,
+  },
+});
+
+describe("DigitalPlanning", () => {
+  describe("getPayload", () => {
+    it("should return valid payload", () => {
+      const instance = new DigitalPlanning({
+        sessionId: "session123",
+        passport: mockPassport,
+        templateNames: ["template1", "template2"],
+      });
+
+      const payload = instance.getPayload();
+
+      expect(payload).toEqual(instance.payload);
+    });
+
+    describe("invalid payloads", () => {
+      test("missing values", () => {
+        const instance = new DigitalPlanning({
+          sessionId: "session123",
+          passport: mockPassport,
+          templateNames: ["template1", "template2"],
+        });
+
+        // @ts-expect-error - The operand of a 'delete' operator must be optional
+        delete instance.payload.data.applicant;
+
+        expect(() => instance.getPayload()).toThrowError(
+          /Invalid DigitalPlanning payload/,
+        );
+      });
+
+      test("undefined values", () => {
+        const instance = new DigitalPlanning({
+          sessionId: "session123",
+          passport: mockPassport,
+          templateNames: ["template1", "template2"],
+        });
+
+        // @ts-expect-error - Type 'undefined' is not assignable to type 'Applicant'
+        instance.payload.data.applicant = undefined;
+
+        expect(() => instance.getPayload()).toThrowError(
+          /Invalid DigitalPlanning payload/,
+        );
+      });
+
+      test("incorrect types", () => {
+        const instance = new DigitalPlanning({
+          sessionId: "session123",
+          passport: mockPassport,
+          templateNames: ["template1", "template2"],
+        });
+
+        // @ts-expect-error - Type 'number' is not assignable to type 'string'
+        instance.payload.data.applicant.name = 12345;
+
+        expect(() => instance.getPayload()).toThrowError(
+          /Invalid DigitalPlanning payload/,
+        );
+      });
+
+      test("incorrect string format", () => {
+        const instance = new DigitalPlanning({
+          sessionId: "session123",
+          passport: mockPassport,
+          templateNames: ["template1", "template2"],
+        });
+
+        instance.payload.metadata.service.url =
+          "not a valid URL, but still a string";
+
+        expect(() => instance.getPayload()).toThrowError(
+          /Invalid DigitalPlanning payload/,
+        );
+      });
+
+      test("incorrect datetime format", () => {
+        const instance = new DigitalPlanning({
+          sessionId: "session123",
+          passport: mockPassport,
+          templateNames: ["template1", "template2"],
+        });
+
+        instance.payload.metadata.session.submittedAt =
+          "not a valid datetime, but still a string";
+
+        expect(() => instance.getPayload()).toThrowError(
+          /Invalid DigitalPlanning payload/,
+        );
+      });
+
+      test("incorrect enum value", () => {
+        const instance = new DigitalPlanning({
+          sessionId: "session123",
+          passport: mockPassport,
+          templateNames: ["template1", "template2"],
+        });
+
+        // @ts-expect-error Type '"invalid enum"' is not assignable to type '"bar" | "baz" | "boo"'
+        instance.payload.files[0].foo = "invalid enum";
+
+        expect(() => instance.getPayload()).toThrowError(
+          /Invalid DigitalPlanning payload/,
+        );
+      });
+    });
+  });
+});
