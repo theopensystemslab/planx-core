17f3bf57df012ef3520019150f887bfff26270aa
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-09-01 09:14:39 +0100

    feat: Improve client auth options

--- src/requests/graphql.ts
+++ src/requests/graphql.ts
@@ -1,25 +1,66 @@
 import { GraphQLClient } from "graphql-request";
 
-export function graphQLClient({
-  url,
-  secret,
-  authorizedSession,
-}: {
-  url: string;
-  secret?: string;
-  authorizedSession?: {
+interface JWTAuth {
+  jwt: string;
+}
+
+interface SessionAuth {
+  session: {
     email: string;
     sessionId: string;
   };
+}
+
+interface AdminAuth {
+  adminSecret: string;
+}
+
+export type Auth = JWTAuth | SessionAuth | AdminAuth;
+
+/**
+ * Client which queries using user's JWT for role-bases access to Hasura
+ * Should be used for all requests made my authorised users
+ */
+const getRoleBasedClient = (url: string, auth: JWTAuth) =>
+  new GraphQLClient(url, { headers: { authorization: `Bearer ${auth.jwt}` } });
+
+/**
+ * Session based client which allows non-authorised users access to their session
+ * Used for Save & Return
+ */
+const getSessionClient = (url: string, auth: SessionAuth) =>
+  new GraphQLClient(url, {
+    headers: {
+      "x-hasura-lowcal-session-id": auth.session.sessionId,
+      "x-hasura-lowcal-email": auth.session.email,
+    },
+  });
+
+/**
+ * Admin client with full access to Hasura
+ * Should be used with caution - only for testing / scripting
+ * @deprecated Consider moving to an "api" role in Hasura
+ */
+const getAdminClient = (url: string, auth: AdminAuth) =>
+  new GraphQLClient(url, {
+    headers: { "x-hasura-admin-secret": auth.adminSecret },
+  });
+
+export function getGraphQLClient({
+  url,
+  auth,
+}: {
+  url: string;
+  auth?: Auth;
 }): GraphQLClient {
-  let headers = {};
-  if (secret) {
-    headers = { "x-hasura-admin-secret": secret };
-  } else if (authorizedSession) {
-    headers = {
-      "x-hasura-lowcal-session-id": authorizedSession.sessionId,
-      "x-hasura-lowcal-email": authorizedSession.email,
-    };
-  }
-  return new GraphQLClient(url, { headers });
+  // Return public client by default
+  if (!auth) return new GraphQLClient(url);
+
+  if ("jwt" in auth) return getRoleBasedClient(url, auth);
+  if ("session" in auth) return getSessionClient(url, auth);
+  if ("adminSecret" in auth) return getAdminClient(url, auth);
+
+  throw Error(
+    "Unable to instantiate GraphQL client - insufficient auth details provided",
+  );
 }
