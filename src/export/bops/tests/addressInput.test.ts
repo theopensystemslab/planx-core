b2c3b2bada9c4f4da51c210f51a81f6a263f1e1b
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-09-04 15:41:15 +0200

    ensure key order for ContactInput AddressInput responses

---
+++ src/export/bops/tests/addressInput.test.ts
@@ -0,0 +1,85 @@
+import { computeBOPSParams } from "../index";
+
+describe("AddressInput details are set correctly based on the breadcrumbs", () => {
+  const testParams = {
+    breadcrumbs: {
+      "u4SPni4RNZ": {
+        "auto": false,
+        "data": {
+          "applicant.address": {
+            "line1": "10 Main Street",
+            "line2": "Apt 1",
+            "town": "Southwark",
+            "county": "London",
+            "postcode": "SE5 0HU",
+            "country": "UK"
+          }
+        }
+      }
+    },
+    passport: {
+      data: {
+        "applicant.address": {
+          "line1": "10 Main Street",
+          "line2": "Apt 1",
+          "town": "Southwark",
+          "county": "London",
+          "postcode": "SE5 0HU",
+          "country": "UK"
+        }
+      }
+    },
+    flow: {
+      "_root": {
+        "edges": [
+          "u4SPni4RNZ"
+        ]
+      },
+      "u4SPni4RNZ": {
+        "type": 130,
+        "data": {
+          "title": "What's your address",
+          "description": "<p>If different than the site address</p>",
+          "fn": "applicant.address"
+        }
+      }
+    },
+    sessionId: "123",
+    flowName: "Apply for a lawful development certificate",
+  };
+
+  it("orders address parts correctly when all fields are present", () => {
+    const generatedPayload = computeBOPSParams(testParams);
+
+    const proposalDetails = generatedPayload.proposal_details;
+    const addressInputItem = proposalDetails?.filter((detail) => detail.question === "What's your address")?.[0];
+
+    expect(addressInputItem?.responses[0]?.["value"]).toEqual("10 Main Street, Apt 1, Southwark, London, SE5 0HU, UK");
+  });
+
+  it("orders address parts correctly when only required fields are present", () => {
+    const generatedPayload = computeBOPSParams({
+      ...testParams,
+      breadcrumbs: {
+        "u4SPni4RNZ": {
+          "auto": false,
+          "data": {
+            "applicant.address": {
+              "line1": "10 Main Street",
+              "line2": "",
+              "town": "Southwark",
+              "county": "",
+              "postcode": "SE5 0HU",
+              "country": ""
+            }
+          }
+        }
+      },
+    });
+
+    const proposalDetails = generatedPayload.proposal_details;
+    const addressInputItem = proposalDetails?.filter((detail) => detail.question === "What's your address")?.[0];
+
+    expect(addressInputItem?.responses[0]?.["value"]).toEqual("10 Main Street, Southwark, SE5 0HU");
+  });
+});
---
+++ src/export/bops/tests/contactInput.test.ts
@@ -0,0 +1,105 @@
+import { computeBOPSParams } from "../index";
+
+describe("ContactInput details are set correctly based on the breadcrumbs", () => {
+  const testParams = {
+    breadcrumbs: {
+      "DQl19JOA9y": {
+        "auto": false,
+        "data": {
+          "_contact.applicant": {
+            "applicant": {
+              "title": "Ms",
+              "firstName": "Jane",
+              "lastName": "Doe",
+              "organisation": "Open Systems Lab",
+              "phone": "0123456789",
+              "email": "test@test.com"
+            }
+          },
+          "applicant.title": "Ms",
+          "applicant.name.first": "Jane",
+          "applicant.name.last": "Doe",
+          "applicant.company.name": "Open Systems Lab",
+          "applicant.phone.primary": "0123456789",
+          "applicant.email": "test@test.com"
+        }
+      }
+    },
+    passport: {
+      "data": {
+        "_contact.applicant": {
+          "applicant": {
+            "title": "Ms",
+            "firstName": "Jane",
+            "lastName": "Doe",
+            "organisation": "Open Systems Lab",
+            "phone": "01 23 45 67 89",
+            "email": "test@test.com"
+          }
+        },
+        "applicant.title": "Ms",
+        "applicant.name.first": "Jane",
+        "applicant.name.last": "Doe",
+        "applicant.company.name": "Open Systems Lab",
+        "applicant.phone.primary": "01 23 45 67 89",
+        "applicant.email": "test@test.com"
+      }
+    },
+    flow: {
+      "_root": {
+        "edges": [
+          "DQl19JOA9y"
+        ]
+      },
+      "DQl19JOA9y": {
+        "type": 135,
+        "data": {
+          "title": "Your contact details",
+          "description": "<p>So we can contact you about your application</p>",
+          "fn": "applicant"
+        }
+      }
+    },
+    sessionId: "123",
+    flowName: "Apply for a lawful development certificate",
+  };
+
+  it("orders contact parts correctly when all fields are present", () => {
+    const generatedPayload = computeBOPSParams(testParams);
+
+    const proposalDetails = generatedPayload.proposal_details;
+    const contactInputItem = proposalDetails?.filter((detail) => detail.question === "Your contact details")?.[0];
+
+    expect(contactInputItem?.responses[0]?.["value"]).toEqual("Ms Jane Doe Open Systems Lab 0123456789 test@test.com");
+  });
+
+  it("orders contact parts correctly when only required fields are present", () => {
+    const generatedPayload = computeBOPSParams({
+      ...testParams,
+      breadcrumbs: {
+        "DQl19JOA9y": {
+          "auto": false,
+          "data": {
+            "_contact.applicant": {
+              "applicant": {
+                "firstName": "Jane",
+                "lastName": "Doe",
+                "phone": "0123456789",
+                "email": "test@test.com"
+              }
+            },
+            "applicant.name.first": "Jane",
+            "applicant.name.last": "Doe",
+            "applicant.phone.primary": "0123456789",
+            "applicant.email": "test@test.com"
+          }
+        }
+      },
+    });
+
+    const proposalDetails = generatedPayload.proposal_details;
+    const contactInputItem = proposalDetails?.filter((detail) => detail.question === "Your contact details")?.[0];
+
+    expect(contactInputItem?.responses[0]?.["value"]).toEqual("Jane Doe 0123456789 test@test.com");
+  });
+});
