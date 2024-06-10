5b8c84085ff01ab8839354036500065e34a63b83
Rory Doak <138574807+RODO94@users.noreply.github.com>
2024-06-10 14:05:13 +0100

    feat: Adding Name Column Changes (#410)
    # Added FlowName to variables for a flow
    
    To match changes I have made in ``planx-new`` I have added the ``name``
    column from the ``flows`` table to the shapes and functions defined here
    
    This has been in order to fix E2E testing failures on ``planx-new`` to
    do with *rory/add-name-editor-2* changes.

--- src/requests/flow.ts
+++ src/requests/flow.ts
@@ -14,6 +14,7 @@ export class FlowClient {
   async create(args: {
     teamId: number;
     slug: string;
+    name: string;
     data?: object;
     status?: FlowStatus;
   }): Promise<string> {
@@ -129,13 +130,20 @@ export async function findPublishedFlowBySessionId(
 
 export async function createFlow(
   client: GraphQLClient,
-  args: { teamId: number; slug: string; data?: object; status?: FlowStatus },
+  args: {
+    teamId: number;
+    slug: string;
+    name: string;
+    data?: object;
+    status?: FlowStatus;
+  },
 ): Promise<string> {
   const response: { insert_flows_one: { id: string } } = await client.request(
     gql`
       mutation CreateFlow(
         $teamId: Int!
         $flowSlug: String!
+        $flowName: String!
         $data: jsonb
         $status: flow_status_enum_enum
       ) {
@@ -143,6 +151,7 @@ export async function createFlow(
           object: {
             team_id: $teamId
             slug: $flowSlug
+            name: $flowName
             data: $data
             version: 1
             status: $status
@@ -155,6 +164,7 @@ export async function createFlow(
     {
       teamId: args.teamId,
       flowSlug: args.slug,
+      flowName: args.name,
       data: args.data,
       status: args.status || "offline",
     },
