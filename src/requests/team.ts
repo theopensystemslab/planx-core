931d4dba9aee3a1f5735bab623cc0d285fd57f83
Rory Doak <rory@opensystemslab.io>
2024-09-03 10:41:46 +0100

    remove submission email from createTeam variables

--- src/requests/team.ts
+++ src/requests/team.ts
@@ -102,7 +102,6 @@ export async function createTeam(
         $name: String!
         $slug: String!
         $domain: String
-        $submissionEmail: String
         $settings: team_settings_insert_input!
         $theme: team_themes_insert_input!
       ) {
@@ -111,12 +110,8 @@ export async function createTeam(
             name: $name
             slug: $slug
             domain: $domain
-            submission_email: $submissionEmail
             # Create empty records for associated tables - these can get populated later
-            team_settings: {
-              data: $settings
-              submission_email: $submissionEmail
-            }
+            team_settings: { data: $settings }
             theme: { data: $theme }
             integrations: { data: {} }
           }
