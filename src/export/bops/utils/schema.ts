cc91e75af1cacfa144523f1396f55d33e261dd96
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-04-07 08:49:10 +0100

    feat: Parse responses from `AddressInput` schema fields (#699)

--- src/export/bops/utils/schema.ts
+++ src/export/bops/utils/schema.ts
@@ -15,21 +15,43 @@ import {
 export const isSchemaType = (type: ComponentType) =>
   [ComponentType.Page, ComponentType.List].includes(type);
 
+const textAndDateInput = z.string();
+
+const numberInput = z.number();
+
+const questionAndChecklistInput = z.array(z.string());
+
+/**
+ * The MapInput field generates a GeoJSON array
+ * Not validated against the full GeoJSON spec, but sufficient for our requirements here
+ */
+const mapInput = z.array(z.record(z.unknown()));
+
+const addressInput = z.object({
+  line1: z.string(),
+  line2: z.string().optional(),
+  town: z.string(),
+  county: z.string().optional(),
+  postcode: z.string(),
+  country: z.string().optional(),
+});
+
+const schemaResponsesSchema = z.union([
+  textAndDateInput,
+  numberInput,
+  questionAndChecklistInput,
+  mapInput,
+  addressInput,
+]);
+
+export type SchemaResponses = z.infer<typeof schemaResponsesSchema>;
+
 /**
  * Parse the breadcrumb (SchemaResponse) which contains the user's responses
  */
 export const parseResponses = (fn: string, crumb: EnrichedCrumb) => {
   const crumbDataSchema = z.object({
-    [fn]: z.array(
-      z.record(
-        z.union([
-          z.string(),
-          z.number(),
-          z.array(z.string()),
-          z.array(z.record(z.unknown())), // GeoJSON array
-        ]),
-      ),
-    ),
+    [fn]: z.array(z.record(schemaResponsesSchema)),
   });
   const crumbData = crumbDataSchema.parse(crumb.data);
   const schemaResponses = crumbData[fn];
