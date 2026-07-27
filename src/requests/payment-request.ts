b10271fcc70e4af6aa0b2e32ec7242bcf66b929b
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-07-27 09:10:54 -0400

    format address value

--- src/requests/payment-request.ts
+++ src/requests/payment-request.ts
@@ -185,14 +185,15 @@ export function extractSessionPreviewData(
   }
   const sessionPreviewData: PaymentRequest["sessionPreviewData"] = {};
   sessionPreviewKeys.forEach((keyPath: KeyPath) => {
+    console.log("HERE", { sessionPreviewKeys, keyPath, passport });
     let value: unknown;
     if (passport.has(keyPath)) {
       value = passport.any(keyPath);
     } else {
       if (keyPath.every((key) => key === "proposal.projectType")) {
         value = ["Not submitted"];
-      } else {
-        value = "Not submitted";
+      } else if (keyPath.every((key) => key === "_address")) {
+        value = { title: "Not submitted" };
       }
     }
 
