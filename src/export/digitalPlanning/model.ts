6f448d9966d7f9466fde84f4679f8a18f650efca
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-08-19 12:06:34 -0400

    remove filter

--- src/export/digitalPlanning/model.ts
+++ src/export/digitalPlanning/model.ts
@@ -915,7 +915,6 @@ export class DigitalPlanning {
 
     if (
       !hasPayComponent ||
-      !this.passport.data?.["application.fee.payable"] ||
       this.applicationType === "ldc.listedBuildingWorks"
     ) {
       return {
